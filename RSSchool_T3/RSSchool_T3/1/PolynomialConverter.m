d5fbd602a031850d380c52c23c033f118c712486
Morau Fiodar <MorauFiodar@yahoo.com>
2020-04-05 11:54:32 +0300

    feat: PolynomialConverter convertToStringFrom... method (Exercise 1)

--- RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
+++ RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
@@ -1,7 +1,60 @@
 #import "PolynomialConverter.h"
 
 @implementation PolynomialConverter
+
 - (NSString*)convertToStringFrom:(NSArray <NSNumber*>*)numbers {
-    return @"";
+    int arraySize = (int)numbers.count;
+    
+    if (numbers.count == 0) {
+        return nil;
+    }
+    
+    NSMutableArray *resultArray = [NSMutableArray array];
+    
+    for (int i = (arraySize - 1); i >= 0; i--) {
+        NSNumber *number = numbers[i];
+        int coef = (arraySize - 1) - i;
+        NSString *stringNumber = [self conertNumberToString:number withCoef:coef];
+        
+        if (stringNumber != nil) {
+            [resultArray addObject:stringNumber];
+            
+            BOOL isLastOne = (i == 0);
+            if (!isLastOne) {
+                NSString *sign = [NSString stringWithFormat:@"%@", number.intValue > 0 ? @"+" : @"-"];
+                [resultArray addObject:sign];
+            }
+        }
+    }
+    
+    NSArray *reversedArray = [[resultArray reverseObjectEnumerator] allObjects];
+    NSString *result = [reversedArray componentsJoinedByString:@" "];
+    
+    return result;
 }
+
+- (NSString *)conertNumberToString:(NSNumber *)number withCoef:(int)coef {
+    if ([number isEqualToNumber:@(0)]) {
+        return nil;
+    }
+    
+    int absValue = abs(number.intValue);
+    
+    if (coef == 0) {
+        return [NSString stringWithFormat:@"%@", @(absValue)];
+    } if (coef == 1) {
+        if (absValue > 1) {
+            return [NSString stringWithFormat:@"%@x", @(absValue)];
+        } else {
+            return [NSString stringWithFormat:@"x"];
+        }
+    } else {
+        if (absValue > 1) {
+            return [NSString stringWithFormat:@"%@x^%@", @(absValue), @(coef)];
+        } else {
+            return [NSString stringWithFormat:@"x^%@", @(coef)];
+        }
+    }
+}
+
 @end
